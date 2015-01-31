a54e6fc2538171cfd0848bb08bbac02c573d87e8
Samuel Simões <slip.cm@gmail.com>
2015-01-31 20:11:48 -0200

    Added the Grunt build steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,5 +15,45 @@ module.exports = function (grunt) {
     }
   });
 
+  config.set("meta.banner",
+    '/*! <%= pkg.name %> v<%= pkg.version %> | ' +
+    '(c) 2014, <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> |' +
+    ' <%= pkg.author.url %> */'
+  );
+
+  config.set("clean.dist.src", ["dist"]);
+
+  config.set("concat.dist", {
+    src: [
+      "src/backbone.offline_attributes.js"
+    ],
+    dest: "dist/backbone.offline_attributes.js",
+    options: {
+      banner: "<%= meta.banner %>\n",
+    }
+  });
+
+  config.set("uglify.dist", {
+    options: {
+      banner: "<%= meta.banner %>",
+      sourceMap: true,
+      sourceMapName: "dist/backbone.offline_attributes.map"
+    },
+
+    files: {
+      "dist/backbone.offline_attributes.min.js": "dist/backbone.offline_attributes.js"
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-mocha");
+
+  config.registerTask("build", [
+    "clean:dist",
+    "concat:dist",
+    "uglify:dist"
+  ]);
+
   grunt.registerTask("test", ["mocha"]);
 };
